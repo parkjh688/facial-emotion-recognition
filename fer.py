471ad0c95b0388b69ffe97528424eb3e329892c8
jhp <jhp@xinapse.ai>
2018-10-11 01:19:26 +0900

    organize directory

--- fer.py
+++ fer.py
@@ -1,7 +1,6 @@
-import sys, os
+import argparse
 import pandas as pd
 import numpy as np
-import cv2
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -10,96 +9,123 @@ from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras.callbacks import ReduceLROnPlateau, TensorBoard, EarlyStopping, ModelCheckpoint
-from keras.models import load_model
 
-BASEPATH = './data'
-sys.path.insert(0, BASEPATH)
-os.chdir(BASEPATH)
-MODELPATH = './models/model.h5'
 
-num_features = 128
-num_labels = 7
-batch_size = 64
-epochs = 100
-width, height = 48, 48
+class TrainFER():
+    def __init__(self, data_path='./data/fer2013.csv', model_path='./models/model.h5'):
+        self.data_path = data_path
+        self.model_path = model_path
 
-data = pd.read_csv('./fer2013.csv')
+        self.num_features = 128
+        self.num_labels = 7
+        self.batch_size = 64
+        self.epochs = 100
+        self.width, self.height = 48, 48
 
-pixels = data['pixels'].tolist() # 1
+        self.data = pd.read_csv(self.data_path)
 
-faces = []
-for pixel_sequence in pixels:
-    face = [int(pixel) for pixel in pixel_sequence.split(' ')] # 2
-    face = np.asarray(face).reshape(width, height) # 3
-    #face = np.asarray(face)
-    #face = face / 255.0 # 4
-    #face = cv2.resize(face.astype('uint8'), (width, height)) # 5
-    faces.append(face.astype('float32'))
+        self.model = self.build_model()
 
-faces = np.asarray(faces)
-faces = np.expand_dims(faces, -1) # 6
+        self.X_train, self.X_val,self.X_test,\
+        self.y_train, self.y_val, self.y_test = self.load_data()
 
-emotions = pd.get_dummies(data['emotion']).as_matrix() # 7
+    def load_data(self):
+        # get label
+        emotions = pd.get_dummies(self.data['emotion']).values
 
-X_train, X_test, y_train, y_test = train_test_split(faces, emotions, test_size=0.1, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=41)
+        pixels = self.data['pixels'].tolist()
 
-model = Sequential()
+        faces = []
+        for pixel_row in pixels:
+            face = [int(pixel) for pixel in pixel_row.split(' ')]
+            face = np.asarray(face).reshape(self.width, self.height)
+            faces.append(face.astype('float32'))
 
-model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
-model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.5))
+        faces = np.asarray(faces)
+        faces = np.expand_dims(faces, -1)
 
-model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.5))
+        X_train, X_test, y_train, y_test = train_test_split(faces, emotions, test_size=0.1, random_state=15)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=24)
 
-model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.5))
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
-model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.5))
+    def build_model(self):
+        model = Sequential()
 
-model.add(Flatten())
+        model.add(Conv2D(self.num_features, kernel_size=(3, 3), activation='relu', input_shape=(self.width, self.height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
+        model.add(Conv2D(self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.5))
 
-model.add(Dense(2*2*2*num_features, activation='relu'))
-model.add(Dropout(0.4))
-model.add(Dense(2*2*num_features, activation='relu'))
-model.add(Dropout(0.4))
-model.add(Dense(2*num_features, activation='relu'))
-model.add(Dropout(0.5))
+        model.add(Conv2D(2*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(2*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.5))
 
-model.add(Dense(num_labels, activation='softmax'))
+        model.add(Conv2D(2*2*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(2*2*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.5))
 
-model.compile(loss=categorical_crossentropy,
-              optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7),
-              metrics=['accuracy'])
+        model.add(Conv2D(2**3*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(2**3*self.num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.5))
 
-lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=3, verbose=1)
+        model.add(Flatten())
 
-tensorboard = TensorBoard(log_dir='./logs')
+        model.add(Dense(2**3*self.num_features, activation='relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(2*2*self.num_features, activation='relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(2*self.num_features, activation='relu'))
+        model.add(Dropout(0.5))
 
-early_stopper = EarlyStopping(monitor='val_loss', min_delta=0, patience=8, verbose=1, mode='auto')
+        model.add(Dense(self.num_labels, activation='softmax'))
 
-checkpointer = ModelCheckpoint(MODELPATH, monitor='val_loss', verbose=1, save_best_only=True)
+        model.compile(loss=categorical_crossentropy,
+                      optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7),
+                      metrics=['accuracy'])
 
-model.fit(np.array(X_train), np.array(y_train),
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(np.array(X_test), np.array(y_test)),
-          shuffle=True,
-          callbacks=[lr_reducer, tensorboard, early_stopper, checkpointer])
+        print(model.summary())
+
+        self.lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=3, verbose=1)
+        self.tensorboard = TensorBoard(log_dir='./logs')
+        self.early_stopper = EarlyStopping(monitor='val_loss', min_delta=0, patience=8, verbose=1, mode='auto')
+        self.checkpointer = ModelCheckpoint(self.model_path, monitor='val_loss', verbose=1, save_best_only=True)
+
+        return model
+
+    def train(self):
+        self.model.fit(np.array(self.X_train), np.array(self.y_train),
+                  batch_size=self.batch_size,
+                  epochs=self.epochs,
+                  verbose=1,
+                  validation_data=(np.array(self.X_val), np.array(self.y_val)),
+                  shuffle=True,
+                  callbacks=[self.lr_reducer, self.tensorboard, self.early_stopper, self.checkpointer])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data', help='Data path', required=False)
+    parser.add_argument('-m', '--model', help='Model path', required=False)
+    args = vars(parser.parse_args())
+
+    if args['data'] and args['model']:
+        tfer = TrainFER(args['data'], args['model'])
+    elif args['data']:
+        tfer = TrainFER(args['data'])
+    elif args['model']:
+        tfer = TrainFER(args['model'])
+    else:
+        tfer = TrainFER()
+
+    tfer.train()
---
+++ real_time_fer.py
@@ -0,0 +1,55 @@
+import argparse
+import numpy as np
+from keras.models import load_model
+import cv2
+
+
+class RealTimeFER():
+	def __init__(self, model_path='./models/model.h5'):
+		self.emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+		self.model = load_model(model_path)
+
+		cv2.namedWindow("preview")
+
+		self.cap = cv2.VideoCapture(0)
+
+	def detect_facial_emotion(self):
+		while(self.cap.isOpened()):
+			ret, frame = self.cap.read()
+
+			if frame is not None:
+				cv2.imshow("preview", frame)
+
+			if ret:
+				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+				face_cascade = cv2.CascadeClassifier('/Users/junghyun/PycharmProjects/facial-emotion-recognition/haarcascade_frontalface_alt.xml')
+				faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+				for (x, y, w, h) in faces:
+					cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+					roi_gray = gray[y:y + h, x:x + w]
+					cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+					cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
+					prediction = self.model.predict(cropped_img)
+					cv2.putText(frame, self.emotion_dict[int(np.argmax(prediction))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+					cv2.imshow('frame', frame)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+		self.cap.release()
+		cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-m', '--model', help='Model path', required=False)
+	args = vars(parser.parse_args())
+
+	if args['model']:
+		rtfer = RealTimeFER(args['model'])
+	else:
+		rtfer = RealTimeFER()
+		rtfer.detect_facial_emotion()
